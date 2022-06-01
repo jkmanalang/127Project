b83e00bda432a7b6f23dae9b8179a54a196785a3
Kenneth <jfmanalang@up.edu.ph>
2022-06-01 14:16:31 +0800

    feature-06 (adding category) completed

--- choices_code.py
+++ choices_code.py
@@ -19,6 +19,7 @@ def markAsDone ():
 
 def addCategory ():
 	print("in add category")
+	functions.addCategory()
 
 def editCategory ():
 	print("in edit category")
--- functions.py
+++ functions.py
@@ -13,6 +13,27 @@ def getUserAnswer():
 		print ("\nInvalid input.")
 	return user_answer
 
+def getIntInput(min_range, max_range, needed_input):
+	while True:
+		try:
+			userInput = int (input (needed_input + " [" + str(min_range) + "-" + str(max_range) + "]: "))
+			while min_range > userInput or userInput > max_range:
+				userInput = int (input (needed_input + " [" + min_range + "-" + max_range + "]: "))
+			break
+		except:
+			print("\nEnter integer within the given range only.")
+	return userInput
+
+def getHighestCategoryNo():
+	select_maxCategoryNo = "SELECT MAX(categoryNo) FROM category"
+	mycursor.execute(select_maxCategoryNo)
+
+	for i in mycursor:
+		highest = i
+		break
+
+	return (highest[0])
+
 def showTasks():
 	print("\n----------------------------- Viewing tasks -----------------------------")
 
@@ -40,4 +61,40 @@ def showTasks():
 				print("\t[" + j[4].strftime("%m/%d/%Y") + "]\t\t" + j[6] + " \t\t " + j[5])
 
 		# print("\n")
+
+
+
+def addCategory():
+	print("\n----------------------------- Adding Category -----------------------------")
+
+	# getting category number
+	categoryNo = getHighestCategoryNo()
+	categoryNo += 1
+
+	# getting category name
+	categoryName = input("Category name: ")
+
+	# getting category type
+	print ("""													
+   [1] Personal
+   [2] Professional
+   [3] Others""")
+
+	categoryTypeNo = getIntInput(1,3,"Category type")
+	if (categoryTypeNo == 1):
+		categoryType = "Personal"
+	elif (categoryTypeNo == 2):
+		categoryType = "Professional"
+	else:
+		categoryType = "Others"
+
+	insertCategoryStatement = "INSERT INTO category (categoryNo, categoryName, categoryType) VALUES (%s, %s, %s)"
+	values = (categoryNo, categoryName, categoryType)
+
+	mycursor.execute(insertCategoryStatement, values)
+
+	mydb.commit()
+	print("Category " + categoryName + " successfully added!")
+
+
 
